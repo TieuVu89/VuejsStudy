6e9adbb1fcbd72f7ac9989bf920d9dc35a83ab41
Tieu Vu <tranchidoanhy@gmail.com>
2020-10-14 15:43:15 +0700

    axios

--- src/components/Axios.vue
+++ src/components/Axios.vue
@@ -1,11 +1,30 @@
 <template>
-  <ul v-if="posts && posts.length">
-    <li v-for="post of posts" v-bind:key="post.id">
-      <p><strong>{{post.title}}</strong></p>
-      <p>{{post.body}}</p>
-    </li>
-  </ul>
+  <div>
+  <table v-if="posts && posts.length">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Phone</th>
+      </tr>
+    </thead>
+    <tbody v-for="post of posts" v-bind:key="post.id">
+    <tr>
+      <td>{{ post.id }}</td>
+      <td>{{ post.name }}</td>
+      <td>{{ post.username }}</td>
+      <td>{{ post.email }}</td>
+      <td>{{ post.address.city }}</td>
+      <td>{{ post.phone }}</td>
+    </tr>
+    </tbody>
 
+  </table>
+
+</div>
   <!-- <ul v-if="errors && errors.length">
     <li v-for="error of errors" v-bind:key="error.message">
       {{error.message}}
@@ -26,7 +45,7 @@ export default {
 
   // lấy dữ liệu khi component được tạo thành công
   created() {
-    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
     .then(response => {
       this.posts = response.data
     })
